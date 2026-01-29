dd64cc8fd9a9a3675cbe5f0e7c575fde655a5783
MoemenAdam <mmuummnna@gmail.com>
2026-01-29 14:30:45 +0200

    Wrap NavigationProgress component with NavigationProvider in layout for improved navigation context management.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-import { NavigationProgress } from 'next-progressbar-link';
+import { NavigationProgress, NavigationProvider } from 'next-progressbar-link';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -28,8 +28,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <NavigationProgress />
-        {children}
+        <NavigationProvider>
+          <NavigationProgress />
+          {children}
+        </NavigationProvider>
       </body>
     </html>
   );
